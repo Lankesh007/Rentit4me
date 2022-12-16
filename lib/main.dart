06ebb0e9717e3cedcdda96c9b7981e1d3fc38404
gajendrapratapsingh <gajendraptsingh05@gmail.com>
2022-12-16 19:46:50 +0530

    IOS notification code

--- lib/main.dart
+++ lib/main.dart
@@ -8,78 +8,87 @@ import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:in_app_update/in_app_update.dart';
 import 'package:rentit4me_new/blocs/network_bloc/network_bloc.dart';
 import 'package:rentit4me_new/utils/dialog_utils.dart';
+import 'package:rentit4me_new/views/PushNotificationService.dart';
 import 'package:rentit4me_new/views/splash_screen.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-const AndroidNotificationChannel channel = AndroidNotificationChannel(
-  "id",
-  "name",
-  importance: Importance.high,
-  playSound: true,
-);
-final navigatorKey = GlobalKey<NavigatorState>();
+// const AndroidNotificationChannel channel = AndroidNotificationChannel(
+//   "id",
+//   "name",
+//   importance: Importance.high,
+//   playSound: true,
+// );
+// final navigatorKey = GlobalKey<NavigatorState>();
 
-final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
-    FlutterLocalNotificationsPlugin();
+// final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+//     FlutterLocalNotificationsPlugin();
 
-Future<void> _firebaseMessangingBackgroundHandler(RemoteMessage message) async {
-  final FlutterLocalNotificationsPlugin flutterLocalNotify =
-      FlutterLocalNotificationsPlugin();
-  var initializationSettingsAndroid =
-      AndroidInitializationSettings('@mipmap/ic_launcher');
-  var initializationSettingsIOS = IOSInitializationSettings();
+// Future<void> _firebaseMessangingBackgroundHandler(RemoteMessage message) async {
+//   final FlutterLocalNotificationsPlugin flutterLocalNotify =
+//       FlutterLocalNotificationsPlugin();
+//   var initializationSettingsAndroid =
+//       AndroidInitializationSettings('@mipmap/ic_launcher');
+//   var initializationSettingsIOS = IOSInitializationSettings();
 
-  var initializationSettings = InitializationSettings(
-      android: initializationSettingsAndroid, iOS: initializationSettingsIOS);
-  flutterLocalNotify.initialize(
-    initializationSettings,
-  );
+//   var initializationSettings = InitializationSettings(
+//       android: initializationSettingsAndroid, iOS: initializationSettingsIOS);
+//   flutterLocalNotify.initialize(
+//     initializationSettings,
+//   );
 
-  log('A new onMessagep event was published!${message.data['title']}     ${message.data['message']}');
+//   log('A new onMessagep event was published!${message.data['title']}     ${message.data['message']}');
 
-  log('Data----->   ${message.data}');
+//   log('Data----->   ${message.data}');
 
-  RemoteNotification notification = message.notification;
-  AndroidNotification android = message.notification.android;
-  if (notification != null && android != null) {
-    flutterLocalNotify.show(
-      notification.hashCode,
-      message.data['title'],
-      message.data['message'],
-      NotificationDetails(
-        android: AndroidNotificationDetails(
-          'channel_ID',
-          'channel name',
-          channelDescription: 'channel description',
-          importance: Importance.max,
-          playSound: true,
-          showProgress: true,
-          priority: Priority.high,
-          ticker: 'test ticker',
-        ),
-      ),
-      payload: json.encode(message.data),
-      // payload: message.data['payload']
-    );
-  } else {
-    log("----->NO data Found !!");
-  }
-}
+//   RemoteNotification notification = message.notification;
+//   AndroidNotification android = message.notification.android;
+//   if (notification != null && android != null) {
+//     flutterLocalNotify.show(
+//       notification.hashCode,
+//       message.data['title'],
+//       message.data['message'],
+//       NotificationDetails(
+//         android: AndroidNotificationDetails(
+//           'channel_ID',
+//           'channel name',
+//           channelDescription: 'channel description',
+//           importance: Importance.max,
+//           playSound: true,
+//           showProgress: true,
+//           priority: Priority.high,
+//           ticker: 'test ticker',
+//         ),
+//       ),
+//       payload: json.encode(message.data),
+//       // payload: message.data['payload']
+//     );
+//   } else {
+//     log("----->NO data Found !!");
+//   }
+// }
 
 Future<void> main() async {
+  
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  FirebaseMessaging.onBackgroundMessage(_firebaseMessangingBackgroundHandler);
-  await flutterLocalNotificationsPlugin
-      .resolvePlatformSpecificImplementation<
-          AndroidFlutterLocalNotificationsPlugin>()
-      ?.createNotificationChannel(channel);
-  await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
-    alert: true,
-    badge: true,
-    sound: true,
-  );
+  await PushNotificationService().setupInteractedMessage();
+  
+  // await Firebase.initializeApp();
+  // FirebaseMessaging.onBackgroundMessage(_firebaseMessangingBackgroundHandler);
+  // await flutterLocalNotificationsPlugin
+  //     .resolvePlatformSpecificImplementation<
+  //         AndroidFlutterLocalNotificationsPlugin>()
+  //     ?.createNotificationChannel(channel);
+  // await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
+  //   alert: true,
+  //   badge: true,
+  //   sound: true,
+  // );
   runApp(MyApp());
+  RemoteMessage initialMessage =
+        await FirebaseMessaging.instance.getInitialMessage();
+    if (initialMessage != null) {
+      // App received a notification when it was killed
+    }
 }
 
 class MyApp extends StatefulWidget {
@@ -97,7 +106,7 @@ class _MyAppState extends State<MyApp> {
 
   @override
   void initState() {
-    showNotify();
+    // showNotify();
     checkForUpdate();
     super.initState();
   }
@@ -215,105 +224,106 @@ class _MyAppState extends State<MyApp> {
         );
   }
 
-  showNotify() async {
-    final FlutterLocalNotificationsPlugin flutterLocalNotify =
-        FlutterLocalNotificationsPlugin();
-    var initializationSettingsAndroid =
-        AndroidInitializationSettings('@mipmap/ic_launcher');
-    var initializationSettingsIOS = IOSInitializationSettings();
+  // showNotify() async {
+  //   final FlutterLocalNotificationsPlugin flutterLocalNotify =
+  //       FlutterLocalNotificationsPlugin();
+  //   var initializationSettingsAndroid =
+  //       AndroidInitializationSettings('@mipmap/ic_launcher');
+  //   var initializationSettingsIOS = IOSInitializationSettings();
 
-    var initializationSettings = InitializationSettings(
-        android: initializationSettingsAndroid, iOS: initializationSettingsIOS);
-    flutterLocalNotify.initialize(initializationSettings,
-        onSelectNotification: onSelectNotification);
+  //   var initializationSettings = InitializationSettings(
+  //       android: initializationSettingsAndroid, iOS: initializationSettingsIOS);
+  //   flutterLocalNotify.initialize(initializationSettings,
+  //       onSelectNotification: onSelectNotification);
 
-    FirebaseMessaging.onMessage.listen(
-      (RemoteMessage message) {
-        log("All data--->${message.notification}");
-        log('As new onMessagep event was published!${message.notification.title}     ${message.notification.body}');
+  //   FirebaseMessaging.onMessage.listen(
+  //     (RemoteMessage message) {
+  //       log("All data--->${message.notification}");
+  //       log('As new onMessagep event was published!${message.notification.title}     ${message.notification.body}');
 
-        // log('Data----->   ${message.data}');
+  //       // log('Data----->   ${message.data}');
 
-        RemoteNotification notification = message.notification;
-        AndroidNotification android = message.notification.android;
-        // log("A--->${message.notification.android}");
-        // log("N--->${message.notification.body}");
+  //       RemoteNotification notification = message.notification;
+  //       AndroidNotification android = message.notification.android;
+  //       // log("A--->${message.notification.android}");
+  //       // log("N--->${message.notification.body}");
 
-        if (notification != null && android != null) {
-          flutterLocalNotify.show(
-            notification.hashCode,
-            message.notification.title,
-            message.notification.body,
-            NotificationDetails(
-              android: AndroidNotificationDetails(
-                'channel_ID',
-                'channel name',
-                channelDescription: 'channel description',
-                importance: Importance.max,
-                playSound: true,
-                showProgress: true,
-                priority: Priority.high,
-                ticker: 'test ticker',
-              ),
-            ),
-            payload: json.encode(message.data),
-            // payload: message.data['payload']
-          );
-        } else {
-          log("No data found!!");
-        }
-        /*   Navigator.of(context).push(
-          MaterialPageRoute(
-            builder: (context) => OrderDetails(
-              deliveryBoyId: message.data['deliveruboyid'].toString(),
-              orderId: message.data['order_id'].toString(),
-              userId: message.data['user_id'].toString(),
-            ),
-          ),
-        ); */
-        // }
-      },
-    );
+  //       if (notification != null && android != null) {
+  //         flutterLocalNotify.show(
+  //           notification.hashCode,
+  //           message.notification.title,
+  //           message.notification.body,
+  //           NotificationDetails(
+  //             android: AndroidNotificationDetails(
+  //               'channel_ID',
+  //               'channel name',
+  //               channelDescription: 'channel description',
+  //               importance: Importance.max,
+  //               playSound: true,
+  //               showProgress: true,
+  //               priority: Priority.high,
+  //               ticker: 'test ticker',
+  //             ),
+  //           ),
+  //           payload: json.encode(message.data),
+  //           // payload: message.data['payload']
+  //         );
+  //       } else {
+  //         log("No data found!!");
+  //       }
+  //       /*   Navigator.of(context).push(
+  //         MaterialPageRoute(
+  //           builder: (context) => OrderDetails(
+  //             deliveryBoyId: message.data['deliveruboyid'].toString(),
+  //             orderId: message.data['order_id'].toString(),
+  //             userId: message.data['user_id'].toString(),
+  //           ),
+  //         ),
+  //       ); */
+  //       // }
+  //     },
+  //   );
 
-    FirebaseMessaging.onMessageOpenedApp.listen(
-      (RemoteMessage message) {
-        log('A new onMessageOpenedApp event was published!');
-        RemoteNotification notification = message.notification;
-        AndroidNotification android = message.notification.android;
+  //   FirebaseMessaging.onMessageOpenedApp.listen(
+  //     (RemoteMessage message) {
+  //       log('A new onMessageOpenedApp event was published!');
+  //       RemoteNotification notification = message.notification;
+  //       AndroidNotification android = message.notification.android;
 
-        if (notification != null && android != null) {
-          flutterLocalNotify.show(
-            notification.hashCode,
-            message.data['title'],
-            message.data['message'],
-            NotificationDetails(
-              android: AndroidNotificationDetails(
-                'channel_ID',
-                'channel name',
-                channelDescription: 'channel description',
-                importance: Importance.max,
-                playSound: true,
-                showProgress: true,
-                priority: Priority.high,
-                ticker: 'test ticker',
-              ),
-            ),
-            payload: json.encode(message.data),
-            // payload: message.data['payload']
-          );
-          /*  Navigator.of(context).push(
-          MaterialPageRoute(
-            builder: (context) => OrderDetails(
-              deliveryBoyId: message.data['deliveruboyid'].toString(),
-              orderId: message.data['order_id'].toString(),
-              userId: message.data['user_id'].toString(),
-            ),
-          ),
-        ); */
-        } else {
-          print("No data");
-        }
-      },
-    );
-  }
+  //       if (notification != null && android != null) {
+  //         flutterLocalNotify.show(
+  //           notification.hashCode,
+  //           message.data['title'],
+  //           message.data['message'],
+  //           NotificationDetails(
+  //             android: AndroidNotificationDetails(
+  //               'channel_ID',
+  //               'channel name',
+  //               channelDescription: 'channel description',
+  //               importance: Importance.max,
+  //               playSound: true,
+  //               showProgress: true,
+  //               priority: Priority.high,
+  //               ticker: 'test ticker',
+  //             ),
+  //           ),
+  //           payload: json.encode(message.data),
+  //           // payload: message.data['payload']
+  //         );
+  //         /*  Navigator.of(context).push(
+  //         MaterialPageRoute(
+  //           builder: (context) => OrderDetails(
+  //             deliveryBoyId: message.data['deliveruboyid'].toString(),
+  //             orderId: message.data['order_id'].toString(),
+  //             userId: message.data['user_id'].toString(),
+  //           ),
+  //         ),
+  //       ); */
+  //       } else {
+  //         print("No data");
+  //       }
+  //     },
+  //   );
+  // }
+
 }
